0cb07a05a9ba0b83ef9e740937e870e97ea1508a
Cameron McQuinn <cameron.mcquinn@gmail.com>
2019-10-21 17:07:32 -0600

    Mount project dir into docker image

--- build.sh
+++ build.sh
@@ -3,7 +3,11 @@
 set -e # exit on error
 set -x # echo commands
 
-echo ${PWD}
-ls -R ${PWD}
-docker pull cmcquinn/snickerdoodle-docker:rootfs
-docker run cmcquinn/snickerdoodle-docker:rootfs /bin/bash ./Recipe
+docker_image=cmcquinn/snickerdoodle-docker:rootfs
+project=snickerdoodle-builder
+
+docker pull ${docker_image}
+docker rm ${project} &> /dev/null || true
+docker run --name ${project} -i \
+     -v "${PWD}:/${project}" ${docker_image} \
+     /bin/bash -c "cd ${project}; ./Recipe"
