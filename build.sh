701ecebbd651e071e127a35f181ca5440b1bc3bd
Cameron McQuinn <cameron.mcquinn@gmail.com>
2019-10-21 16:59:14 -0600

    Slowly putting Travis CI bits and pieces in place

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+
+set -e # exit on error
+set -x # echo commands
+
+echo ${PWD}
+ls -R ${PWD}
 docker pull cmcquinn/snickerdoodle-docker:rootfs
 docker run cmcquinn/snickerdoodle-docker:rootfs /bin/bash ./Recipe
