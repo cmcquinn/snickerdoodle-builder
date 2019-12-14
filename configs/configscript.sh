e8c5a911a0ae9b5c101b1a360e62efc184a8bf2d
Cameron McQuinn <cameron.mcquinn@gmail.com>
2019-12-14 16:19:11 -0700

    Fix base-files dependency on base-passwd

--- configs/configscript.sh
+++ configs/configscript.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+set -x
 export DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true
 export LC_ALL=C LANGUAGE=C LANG=C
+/var/lib/dpkg/info/base-passwd.preinst install
 /var/lib/dpkg/info/bash.preinst install
 if [ -d /tmp/preseeds/ ]; then
         for file in `ls -1 /tmp/preseeds/*`; do
