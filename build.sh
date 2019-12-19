38bbca01527587644f9c2c9cb2a8fcb8e30768d4
Cameron McQuinn <cameron.mcquinn@gmail.com>
2019-12-19 16:13:41 -0700

    Disable Travis CI caching

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-set -e # exit on error
 set -x # echo commands
 
 docker_image=cmcquinn/snickerdoodle-docker:rootfs
@@ -13,20 +12,12 @@ docker pull ${docker_image}
 docker rm ${project} &> /dev/null || true
 cd ${TRAVIS_BUILD_DIR}/.. # cd to /home/travis/build/cmcquinn
 mkdir -p work
-mkdir -p cache
 workdir=$(realpath work)
-cachedir=$(realpath cache)
-
-# if cache exists, restore rootfs from it
-if [ -f ${cachedir}/cache.tgz ]; then
-     cp ${cachedir}/cache.tgz ${workdir}
-     cd ${workdir}
-     tar xf cache.tgz
-fi
-
 cd ${TRAVIS_BUILD_DIR}
 docker run --privileged --name ${project} -i \
      -v "${PWD}:/${project}" -v "${workdir}:/work" ${docker_image} \
      /bin/bash -c "cd ${project}; ./Recipe"
-sudo tar -zcf ${cachedir}/cache.tgz ${workdir}/multistrap 
-sudo chown $USER:$USER ${cachedir}/cache.tgz # allow access to cache archive by travis user
+
+git clone https://github.com/cmcquinn/python-utils.git
+pip3 install requests
+python-utils/bintray.py cmcquinn replicookie-rootfs debian-buster ${workdir}/rootfs.tar.xz
